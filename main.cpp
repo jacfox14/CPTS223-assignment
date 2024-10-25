d0083cc0318a968360d31a2d4cbb5094cf440f5e
jacfox14 <jacfox14@gmail.com>
2024-10-25 15:28:35 -0700

    MA3 final commit

---
+++ main.cpp
@@ -0,0 +1,197 @@
+#include <map>
+#include "generateUsers.hpp"
+using namespace std;
+
+
+map<string, User> buildMapByUserName(vector<User> users) {
+    map<string, User> usersMap;
+    //cout << "TODO" << endl;
+
+    for (const auto& user : users) {
+        usersMap[user.userName] = user;
+    }
+
+    return usersMap;
+}
+
+
+map<string, User> buildMapByEmail(vector<User> users) {
+    map<string, User> usersMap;
+    
+    //cout << "TODO" << endl;
+    for (const auto& user : users) {
+        usersMap[user.email] = user;
+    }
+
+    return usersMap;
+}
+
+
+void printMap(map<string, User> aMap) {
+    cout << "TODO" << endl;
+
+    for (const auto& [key, value] : aMap) {
+        cout << key << " : " << value.userName << endl;
+    }
+}
+
+
+bool isMapSorted(map<string, User> aMap) {
+    
+    //cout << "TODO" << endl;
+    if (aMap.empty()) {
+        return true;
+    }
+
+    auto it = aMap.begin();
+    auto prev = it;
+    ++it;
+
+    while (it != aMap.end()) {
+        if (prev->first > it->first) {
+            return false;
+        }
+        prev = it;
+        ++it;
+    }
+
+    return true;
+}
+
+
+bool testSearchByKey(map<string, User> aMap, string keyToSearch) {
+    cout << "TODO" << endl;
+    return aMap.find(keyToSearch) != aMap.end();
+}
+
+bool testDeleteByKey(map<string, User> aMap, string keyToDelete) {
+    cout << "TODO" << endl;
+
+    if ( aMap.find(keyToDelete) != aMap.end() ) {
+        aMap.erase(keyToDelete);
+        return true;
+    }
+
+    return false;
+}
+
+
+void printActiveUsers(map<string, User> aMap) {
+    int activeThreshold = 800;
+    cout << "TODO" << endl;
+
+
+    for (const auto& [key, value] : aMap) {
+        if ( value.numPosts > activeThreshold ) {
+            cout << key << " : " << value.numPosts << endl;
+        }
+    }
+
+}
+
+
+void printMostPopularCategory(map<string, User> aMap) {    
+    cout << "TODO" << endl;
+    map<string, int> categoryCount;
+
+    // Count occurrences of each category
+    for (const auto& [key, value] : aMap) {
+        categoryCount[value.mostViewedCategory]++;
+    }
+
+    // Find the category with the highest count
+    string mostPopularCategory;
+    int maxCount = 0;
+    for (const auto& [category, count] : categoryCount) {
+        if (count > maxCount) {
+            maxCount = count;
+            mostPopularCategory = category;
+        }
+    }
+
+    // Print the most popular category
+    cout << "Most Popular Category: " << mostPopularCategory << endl;
+
+}
+
+
+int main()
+{
+    int numUsers = 10;
+    vector<User> users = generateUsers(numUsers);
+
+
+    cout << "Build map with username as key" << endl;
+    map<string, User> mapByUserName = buildMapByUserName(users);
+    if ( mapByUserName.size() == numUsers )
+        cout << "  Built successfully." << endl << endl;
+    else
+        cout << "  Built unsuccessfully." << endl << endl;
+
+
+    cout << "Print \"mapByUserName\" map:" << endl;
+    printMap(mapByUserName);
+    cout << endl;
+
+
+    string keyToSearch = "smith55";
+    cout << "Search by key: mapByUserName[\"" << keyToSearch << "\"]" << endl;
+    if ( testSearchByKey(mapByUserName, keyToSearch) )
+        cout << "  Search successfully." << endl << endl;
+    else
+        cout << "  Search unsuccessfully." << endl << endl;
+
+
+    string keyToDelete = "smith55";
+    cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+    if ( testDeleteByKey(mapByUserName, keyToDelete) )
+        cout << "  Delete successfully." << endl << endl;
+    else
+        cout << "  Delete unsuccessfully." << endl << endl;
+
+
+    cout << "Test if map's key is sorted" << endl;
+    if ( isMapSorted(mapByUserName) )
+        cout << "  Order test passed!" << endl << endl;
+    else
+        cout << "  Order test failed!" << endl << endl;
+
+
+    cout << "Print usernames with more than 800 tweets:" << endl;
+    printActiveUsers(mapByUserName);
+    cout << endl;
+
+    cout << "Print the most popular category" << endl;
+    printMostPopularCategory(mapByUserName);
+    cout << endl;
+
+
+    cout << " ============================================================================== " << endl << endl;
+
+
+    cout << "Build map with username as key" << endl;
+    map<string, User> mapByEmail = buildMapByEmail(users);
+    if ( mapByEmail.size() == numUsers )
+        cout << "  Built successfully." << endl << endl;
+    else
+        cout << "  Built unsuccessfully." << endl << endl;
+    
+
+    keyToSearch = "kat@gmail.com";
+    cout << "Search by key: mapByEmail[\"" << keyToSearch << "\"]" << endl;
+    if ( testSearchByKey(mapByEmail, keyToSearch) )
+        cout << "  Search successfully." << endl << endl;
+    else
+        cout << "  Search unsuccessfully." << endl << endl;
+
+
+    keyToDelete = "kat@gmail.com";
+    cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+    if ( testDeleteByKey(mapByEmail, keyToDelete) )
+        cout << "  Delete successfully." << endl << endl;
+    else
+        cout << "  Delete unsuccessfully." << endl << endl;
+
+
+    return 0;
+}
